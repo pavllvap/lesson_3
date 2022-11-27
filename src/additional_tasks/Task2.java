1663526aa8ebbead1e8db329a87ff540703431f3
user <you@example.com>
2022-11-28 02:54:49 +0300

    change calculations

--- src/additional_tasks/Task2.java
+++ src/additional_tasks/Task2.java
@@ -10,15 +10,14 @@ public class Task2 {
     public static void countDeposit() {
         Scanner scanner1 = new Scanner(System.in);
         Scanner scanner2 = new Scanner(System.in);
-        float sum = 0f;
         System.out.println("Enter deposit : ");
         float deposit = scanner1.nextFloat();
         System.out.println("Enter amount of months : ");
         int months = scanner2.nextInt();
         for (int i = 1; i <= months; i++) {
-            sum = (float) (sum + deposit * 1.07);
+            deposit = (float) (deposit * 1.07);
         }
-        System.out.println(sum);
+        System.out.println(deposit);
     }
 
 
